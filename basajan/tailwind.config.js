32fce0fdcc3d08dd637c45fea29499bf0c8719f6
Ahmad Husen <hi@husen.id>
2022-03-05 12:40:57 +0700

    chore(basajan): improve whitespace

--- basajan/tailwind.config.js
+++ basajan/tailwind.config.js
@@ -18,8 +18,42 @@ module.exports = {
       white: colors.white,
       gray: colors.coolGray,
     },
+		/*
+		fontSize: {
+			'xs': '0.75rem',
+			'sm': '0.875rem',
+			'base': '1rem',
+			'lg': '1.125rem',
+			'xl': '1.25rem',
+			'2xl': '1.5rem',
+			'3xl': '1.875rem',
+			'4xl': '2.25rem',
+			'5xl': '3rem',
+			'6xl': '3.75rem',
+			'7xl': '4.5rem',
+			'8xl': '6rem',
+			'9xl': '8rem',
+		},
+		*/
 		screens: {},
-		extend: {},
+		extend: {
+			spacing: {
+				18: '4.5rem',
+			},
+			// fontSize: {
+			// 	'3xl': [
+			// 		'1.875rem', {
+			// 			lineHeight: '1.375',
+			// 		},
+			// 	],
+			// 	'4xl': ['2.25rem'],
+			// 	'5xl': '3rem',
+			// 	'6xl': '3.75rem',
+			// 	'7xl': '4.5rem',
+			// 	'8xl': '6rem',
+			// 	'9xl': '8rem',
+			// },
+		},
 	},
 	variants: {
 		extend: {},
