0fafb034a84c8b994bdda34411df4c5d6e1b0dc9
Ahmad Husen <hi@husen.id>
2022-03-05 19:56:02 +0700

    refactor(basajan): adjust line-height (leading)

--- basajan/tailwind.config.js
+++ basajan/tailwind.config.js
@@ -18,41 +18,78 @@ module.exports = {
       white: colors.white,
       gray: colors.coolGray,
     },
-		/*
 		fontSize: {
-			'xs': '0.75rem',
-			'sm': '0.875rem',
-			'base': '1rem',
-			'lg': '1.125rem',
-			'xl': '1.25rem',
-			'2xl': '1.5rem',
-			'3xl': '1.875rem',
-			'4xl': '2.25rem',
-			'5xl': '3rem',
-			'6xl': '3.75rem',
-			'7xl': '4.5rem',
-			'8xl': '6rem',
-			'9xl': '8rem',
+			'xs': [
+				'0.75rem', {
+					lineHeight: 1.5,
+				},
+			],
+			'sm': [
+				'0.875rem', {
+					lineHeight: 1.5,
+				},
+			],
+			'base': [
+				'1rem', {
+					lineHeight: 1.5,
+				},
+			],
+			'lg': [
+				'1.125rem', {
+					lineHeight: 1.5,
+				},
+			],
+			'xl': [
+				'1.25rem', {
+					lineHeight: 1.375,
+				}
+			],
+			'2xl': [
+				'1.5rem', {
+					lineHeight: 1.375,
+				},
+			],
+			'3xl': [
+				'1.875rem', {
+					lineHeight: 1.375,
+				},
+			],
+			'4xl': [
+				'2.25rem', {
+					lineHeight: 1.375,
+				},
+			],
+			'5xl': [
+				'3rem', {
+					lineHeight: 1.25,
+				},
+			],
+			'6xl': [
+				'3.75rem', {
+					lineHeight: 1.25,
+				},
+			],
+			'7xl': [
+				'4.5rem', {
+					lineHeight: 1.25,
+				},
+			],
+			'8xl': [
+				'6rem', {
+					lineHeight: 1.25,
+				},
+			],
+			'9xl': [
+				'8rem', {
+					lineHeight: 1.25,
+				},
+			],
 		},
-		*/
 		screens: {},
 		extend: {
 			spacing: {
 				18: '4.5rem',
 			},
-			// fontSize: {
-			// 	'3xl': [
-			// 		'1.875rem', {
-			// 			lineHeight: '1.375',
-			// 		},
-			// 	],
-			// 	'4xl': ['2.25rem'],
-			// 	'5xl': '3rem',
-			// 	'6xl': '3.75rem',
-			// 	'7xl': '4.5rem',
-			// 	'8xl': '6rem',
-			// 	'9xl': '8rem',
-			// },
 		},
 	},
 	variants: {
